5434a9621a79d8dce48ee96b57a3d788c12c0dc0
Catherine <wambukukiiru@gmail.com>
2022-10-04 22:46:21 +0300

    more changes to the 2nd CTA component

--- components/CtaBanner.tsx
+++ components/CtaBanner.tsx
@@ -5,39 +5,29 @@ type PropsWithClassName<T = {}> = { className?: string } & T
 function CtaBanner ({className} : PropsWithClassName) {
     return (
         <section>
-            <section>
-        <div className="flex bg-Primary py-12 px-12"> 
-           
+        <div className="flex bg-Primary py-12 px-36">
             <div className="container flex">
-                <div className="p-4 ">
-                    <h1 className="text-center md:text-left text-6xl font-light text-white">
+                <div className="p-4 mb-24">
+                    <h1 className="text-6xl font-light text-white">
                         Get inspired, empowered and motivated.
                     </h1>
                 </div>  
             </div>
-            <div className="mt-4">    
-            <a href="#" className="hidden md:block flex p-3 px-16 text-white border-2 border-white text-white rounded-full">
-                    Be a member
-                </a>
-            </div>
-            
-        </div>
-        <div className="container flex drop-shadow-lg p-16 rounded-lg bg-white">
-            <div className="">
-                <h1 className="text-4xl text-Primary font-light">
-                    Invest in the next generation of techies.
-                </h1>
-            </div>
-            <div className="">
-                <a href="#" className="hidden md:block flex text-Primary p-3 px-16 border-2 border-Primary text-white rounded-full">
-                    Be a partner
-                </a>
+            <CtaButton>Be a Member</CtaButton>
+            <div className=" absolute container flex flex-row drop-shadow-lg p-16 rounded-lg mt-48 bg-white">
+                <div className="container flex">
+                    <h1 className="text-4xl text-Primary font-light">
+                        Invest in the next generation of techies.
+                    </h1>
+                </div>
+                <div className="flex">
+                    <a href="#" className="hidden md:block text-Primary p-3 px-16 border-2 border-Primary text-white rounded-full">
+                        Be a partner
+                    </a>
+                </div>
             </div>
         </div>
-
     </section>
-        </section>
     )
 } 
-
 export {CtaBanner as default}
